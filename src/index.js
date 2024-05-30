04cab8fde7a83bbc18c967286e899202dd150d5f
Tae Woo Kim <taewookim02@gmail.com>
2024-05-30 16:09:14 +0900

    feat: Add next, prev button functionality

--- src/index.js
+++ src/index.js
@@ -8,3 +8,51 @@ import dotInactive from "./img/dot-inactive.svg";
 import arrowRight from "./img/arrow-right.svg";
 import arrowLeft from "./img/arrow-left.svg";
 import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const nextBtn = document.querySelector(".next-slide");
+  const prevBtn = document.querySelector(".prev-slide");
+  const slides = document.querySelector(".slides");
+  const slideImgs = document.querySelectorAll(".slide");
+  const maxLen = slideImgs.length;
+  const dotDivs = document.querySelectorAll(".dot");
+  const svgEls = document.querySelectorAll(".dot-svg");
+
+  let index = 0;
+
+  const resetDotActive = () => {
+    dotDivs.forEach((el) => {
+      el.classList.remove("dot-active");
+    });
+  };
+
+  const updateSlidePosition = () => {
+    slides.style.transform = `translateY(-50%) translateX(${index * -100}%)`;
+  };
+
+  const updateDotActive = () => {
+    resetDotActive();
+    const target = document.querySelector(`div[data-order='${index}']`);
+    target.classList.add("dot-active");
+  };
+  nextBtn.addEventListener("click", () => {
+    if (index < maxLen - 1) {
+      index++;
+    } else {
+      index = 0;
+    }
+    updateSlidePosition();
+    updateDotActive();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    if (index > 0) {
+      index--;
+    } else {
+      index = maxLen - 1;
+    }
+    updateSlidePosition();
+    updateDotActive();
+    console.log(index);
+  });
+});
